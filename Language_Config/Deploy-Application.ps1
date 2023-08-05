48648a9b4d3d2b304bf26e03421ed36c44d6ad46
Nick Jenkins <n.jenkins@outlook.com>
2023-08-05 10:18:20 +0100

    updated

--- Language_Config/Deploy-Application.ps1
+++ Language_Config/Deploy-Application.ps1
@@ -115,7 +115,7 @@ Try {
     [String]$appRevision = '01'
     [String]$appScriptVersion = '1.0.0'
     [String]$appScriptDate = '26/07/20XX'
-    [String]$appScriptAuthor = 'Mark Ellis'
+    [String]$appScriptAuthor = ''
     ##*===============================================
     ## Variables: Install Titles (Only set here to override defaults set by the toolkit)
     [String]$installName = ''
